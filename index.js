a22cc7a4bb6054e4f06596b3d5f1434a0022e46e
Thiago Arrais <thiago.souza@serpro.gov.br>
2018-02-05 14:29:25 -0300

    bogus endpoint for receiving email requests

--- index.js
+++ index.js
@@ -24,6 +24,29 @@ app.post('/', cors({ origin: process.env.NODE_ENV === 'development' ? 'http://lo
     })
 })
 
+app.post('/send', cors({ origin: process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://heml.io' }), function (req, res) {
+  return HEML(req.body.heml || '')
+    .then((results) => {
+
+      if (results.errors) {
+        results.errors = results.errors.map((error) => {
+          return { selector: error.selector, message: error.toString() }
+        })
+      }
+
+      res.status(200).send("OK")
+
+      return results;
+    })
+    .then((heml) => {
+      // FIXME: This is where we will call sparkpost
+      console.log("doing the magic...")
+      console.log(req.body.email)
+      console.log(heml.html)
+      console.log("the magic is done!")
+    })
+})
+
 app.listen(process.env.PORT || 5000, function () {
   console.log(`listening on port ${process.env.PORT || 5000}!`)
 })
